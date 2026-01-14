074b1d8f756083612dcd665eb19b65841653fea4
adithya-adee <adithya25905@gmail.com>
2026-01-15 03:48:18 +0530

    Remove anchor program

--- programs/shadowlend-contracts/src/state/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod pool;
-pub mod user_obligation;
-pub mod arcium_config;
-
-pub use pool::*;
-pub use user_obligation::*;
-pub use arcium_config::*;
--- programs/shadowlend-contracts/src/utils/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod interest_rate;
-pub mod attestation;
-pub mod arcium_integration;
-
-pub use interest_rate::*;
-pub use attestation::*;
-pub use arcium_integration::*;
